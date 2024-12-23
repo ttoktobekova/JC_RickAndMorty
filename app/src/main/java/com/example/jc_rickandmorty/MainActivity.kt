21fbb393e09c69b94909e39d9199fbee7c54e9c8
ttoktobekova <gul775653@gmail.com>
2024-12-23 18:32:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/jc_rickandmorty/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.jc_rickandmorty
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jc_rickandmorty.ui.theme.JC_RickAndMortyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JC_RickAndMortyTheme {
+
+            }
+        }
+    }
+}
